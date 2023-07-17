39694f51a5a8f666822af996aa2a3e60d77909c1
omensann <135625902+omensann@users.noreply.github.com>
2023-07-17 11:13:50 +0000

    [add]Meshiterro

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/about' => 'homes#about'
   root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:edit, :show, :update]
